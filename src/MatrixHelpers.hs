e3bb56601f4f7f6e7c92775566e513d23a8421bf
Chris Pollard <cspollard@gmail.com>
2017-02-02 14:06:26 +0000

    more progress!

--- src/MatrixHelpers.hs
+++ src/MatrixHelpers.hs
@@ -8,6 +8,8 @@ module MatrixHelpers
   , toVectorM
   , fromVectorM
   , reifyMatrix
+  , reifyMatrix1
+  , reifyMatrix2
   ) where
 
 import           Data.Proxy
@@ -36,3 +38,26 @@ reifyMatrix m f = do
       \(Proxy :: Proxy k2) -> do
         (mV :: M k2 k1 a) <- fromVectorM m
         return $ f mV
+
+
+reifyMatrix1
+  :: forall n a r. Dim n
+  => Vector (Vector a) -> (forall m. Dim m => M n m a -> r) -> Maybe r
+reifyMatrix1 m f = do
+  let l1 = reflectDim (Proxy :: Proxy n)
+      l2 = if l1 == 0 then 0 else V.length (m V.! 0)
+  reifyDim l2 $
+    \(Proxy :: Proxy k2) -> do
+      (mV :: M n k2 a) <- fromVectorM m
+      return $ f mV
+
+
+reifyMatrix2
+  :: forall m a r. Dim m
+  => Vector (Vector a) -> (forall n. Dim m => M n m a -> r) -> Maybe r
+reifyMatrix2 m f = do
+  let l1 = V.length m
+  reifyDim l1 $
+    \(Proxy :: Proxy k1) -> do
+      (mV :: M k1 m a) <- fromVectorM m
+      return $ f mV
